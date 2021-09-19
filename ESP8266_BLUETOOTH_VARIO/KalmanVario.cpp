1a67e2c7e1f2529b3b458fda4442d609c55973fc
Hari Nair <hair.nair@gmail.com>
2021-09-19 12:10:13 +0530

    update kf3

--- ESP8266_BLUETOOTH_VARIO/KalmanVario.cpp
+++ ESP8266_BLUETOOTH_VARIO/KalmanVario.cpp
@@ -13,10 +13,10 @@
 // This can be calculated offline for the specific sensor.
 // zInitial can be determined by averaging a few samples of the altitude measurement.
 // zAccelBiasVariance can be set low as it is not expected to drift much
-// vInitial and aBiasInitial can be set as 0.0
+// vInitial can be set to 0 if not known
 
 
-void KalmanVario::Config(float zMeasVariance, float zAccelVariance, float zAccelBiasVariance, float zInitial, float vInitial, float aBiasInitial) {
+void KalmanVario::Config(float zMeasVariance, float zAccelVariance, float zAccelBiasVariance, float zInitial, float vInitial) {
 #ifdef KF_DEBUG
 	Serial.printf("zMeasVariance %d\r\nzAccelVariance %d\r\n", (int)zMeasVariance, (int)zAccelVariance);
 #endif	
@@ -26,37 +26,34 @@ void KalmanVario::Config(float zMeasVariance, float zAccelVariance, float zAccel
 
 	z_ = zInitial;
 	v_ = vInitial;
-	aBias_ = aBiasInitial;
-	Pzz_ = 1.0f;
+	aBias_ = 0.0f; // initial estimate for bias is 0
+	
+	Pzz_ = 400.0f;
 	Pzv_ = 0.0f;
 	Pza_ = 0.0f;
 	
-	Pvz_ = 0.0f;
-	Pvv_ = 1.0f;
+	Pvz_ = Pzv_;
+	Pvv_ = 400.0f;
 	Pva_ = 0.0f;
 	
-	Paz_ = 0.0f;
-	Pav_ = 0.0;
-	Paa_ = 100000.0f;
+	Paz_ = Pza_;
+	Pav_ = Pva_;
+	Paa_ = 400.0f;
 	}
 
-
-// Updates state [Z, V] given a sensor measurement of z, acceleration a, 
-// and the time in seconds dt since the last measurement. 
-
-void KalmanVario::Update(float z, float a, float dt, float* pZ, float* pV) {
-
-	// Predict state
-    float accel = a - aBias_;
-	v_ += accel * dt;
-	z_ += v_ * dt;
-
+// gravity-compensated earth-z accel in cm/s/s,  and elapsed time dt in second
+void KalmanVario::Predict(float a, float dt) {
+	// Predicted (a priori) state vector estimate x_k- = F * x_k-1+
+	float accel_true = a - aBias_; // true acceleration = measured acceleration minus acceleration sensor bias
+	z_ = z_ + (v_ * dt);
+	v_ = v_ + (accel_true * dt);
+	
     // Predict State Covariance matrix
 	float t00,t01,t02;
     float t10,t11,t12;
     float t20,t21,t22;
 	
-    float dt2div2 = dt*dt/2.0f;
+    float dt2div2 = dt*dt*0.5f;
     float dt3div2 = dt2div2*dt;
     float dt4div4 = dt2div2*dt2div2;
 	
@@ -76,13 +73,15 @@ void KalmanVario::Update(float z, float a, float dt, float* pZ, float* pV) {
 	Pzv_ = t01 - dt*t02;
 	Pza_ = t02;
 	
-	Pvz_ = t10 + dt*t11 - dt2div2*t12;
+	Pvz_ = Pzv_;
 	Pvv_ = t11 - dt*t12;
 	Pva_ = t12;
 	
-	Paz_ = t20 + dt*t21 - dt2div2*t22;
-	Pav_ = t21 - dt*t22;
+	Paz_ = Pza_;
+	Pav_ = Pva_;
 	Paa_ = t22;
+	
+	// add Q_k
 
     Pzz_ += dt4div4*zAccelVariance_;
     Pzv_ += dt3div2*zAccelVariance_;
@@ -91,7 +90,12 @@ void KalmanVario::Update(float z, float a, float dt, float* pZ, float* pV) {
     Pvv_ += dt*dt*zAccelVariance_;
 
     Paa_ += zAccelBiasVariance_;
+	}
 
+// Updates state [Z, V] given a sensor measurement of z, acceleration a, 
+// and the time in seconds dt since the last measurement. 
+
+void KalmanVario::Update(float z, float dt, float* pZ, float* pV) {
 	// Error
 	float innov = z - z_; 
 	float sInv = 1.0f / (Pzz_ + zMeasVariance_);  
@@ -104,10 +108,7 @@ void KalmanVario::Update(float z, float a, float dt, float* pZ, float* pV) {
 	// Update state 
 	z_ += kz * innov;
 	v_ += kv * innov;
-	aBias_ += ka * innov;
-	
-	*pZ = z_;
-	*pV = v_;
+	aBias_ += ka * innov;	
 
 	// Update state covariance matrix
 	Paz_ -= ka * Pzz_;
@@ -121,5 +122,8 @@ void KalmanVario::Update(float z, float a, float dt, float* pZ, float* pV) {
 	Pzz_ -= kz * Pzz_;
 	Pzv_ -= kz * Pzv_;
 	Pza_ -= kz * Pza_;
+
+	*pZ = z_;
+	*pV = v_;
 	}
 
